15026df350cd2ab4e3ce5f58528b5e61a2e82f9a
Jeremy Bernard <jeremy.bernard@iex.ec>
2025-08-01 09:59:35 +0200

    style: update newline handling in logs

--- cvmassistants/secretprovider/secret-provider-agent/src/secret_provider_agent.c
+++ cvmassistants/secretprovider/secret-provider-agent/src/secret_provider_agent.c
@@ -22,7 +22,7 @@
             struct tm *t = gmtime(&now); \
             char ts[24]; \
             strftime(ts, sizeof(ts), "%Y-%m-%d %H:%M:%S UTC", t); \
-            printf("%-29s [%-5s] [%s:%d] " fmt, ts, level, __FILE__, __LINE__, ##__VA_ARGS__); \
+            printf("%-29s [%-5s] [%s:%d] " fmt "\n", ts, level, __FILE__, __LINE__, ##__VA_ARGS__); \
         } \
     } while (0)
 
@@ -54,25 +54,25 @@ char* get_secret_from_kbs_through_rats_tls(rats_tls_log_level_t log_level,
 
     bool validation_error = false;
     if (attester_type == NULL || strlen(attester_type) >= ENCLAVE_ATTESTER_TYPE_NAME_SIZE) {
-        LOG_ERROR("attester_type is NULL or exceeds maximum allowed size (%d)\n",
+        LOG_ERROR("attester_type is NULL or exceeds maximum allowed size (%d)",
             ENCLAVE_ATTESTER_TYPE_NAME_SIZE - 1);
         validation_error = true;
     }
 
     if (verifier_type == NULL || strlen(verifier_type) >= ENCLAVE_VERIFIER_TYPE_NAME_SIZE) {
-        LOG_ERROR("verifier_type is NULL or exceeds maximum allowed size (%d)\n",
+        LOG_ERROR("verifier_type is NULL or exceeds maximum allowed size (%d)",
             ENCLAVE_VERIFIER_TYPE_NAME_SIZE - 1);
         validation_error = true;
     }
 
     if (tls_type == NULL || strlen(tls_type) >= TLS_TYPE_NAME_SIZE) {
-        LOG_ERROR("tls_type is NULL or exceeds maximum allowed size (%d)\n",
+        LOG_ERROR("tls_type is NULL or exceeds maximum allowed size (%d)",
             TLS_TYPE_NAME_SIZE - 1);
         validation_error = true;
     }
 
     if (crypto_type == NULL || strlen(crypto_type) >= CRYPTO_TYPE_NAME_SIZE) {
-        LOG_ERROR("crypto_type is NULL or exceeds maximum allowed size (%d)\n",
+        LOG_ERROR("crypto_type is NULL or exceeds maximum allowed size (%d)",
             CRYPTO_TYPE_NAME_SIZE - 1);
         validation_error = true;
     }
@@ -95,7 +95,7 @@ char* get_secret_from_kbs_through_rats_tls(rats_tls_log_level_t log_level,
             conf.custom_claims = (claim_t*)custom_claims;
             conf.custom_claims_length = 1;
         } else {
-            LOG_ERROR("could not read the app_id from env\n");
+            LOG_ERROR("could not read the app_id from env");
             return NULL;
         }
     }
@@ -119,7 +119,7 @@ char* get_secret_from_kbs_through_rats_tls(rats_tls_log_level_t log_level,
      */
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        LOG_ERROR("failed to call socket()\n");
+        LOG_ERROR("failed to call socket()");
         return NULL;
     }
     struct sockaddr_in s_addr;
@@ -129,32 +129,32 @@ char* get_secret_from_kbs_through_rats_tls(rats_tls_log_level_t log_level,
 
     /* Get the server IPv4 address from the command line call */
     if (inet_pton(AF_INET, ip, &s_addr.sin_addr) != 1) {
-        LOG_ERROR("invalid server address\n");
+        LOG_ERROR("invalid server address");
         close(sockfd);
         return NULL;
     }
 
     /* Connect to the server */
     if (connect(sockfd, (struct sockaddr*)&s_addr, sizeof(s_addr)) == -1) {
-        LOG_ERROR("failed to call connect()\n");
+        LOG_ERROR("failed to call connect()");
         close(sockfd);
         return NULL;
     }
     rats_tls_handle handle;
     rats_tls_err_t ret = rats_tls_init(&conf, &handle);
     if (ret != RATS_TLS_ERR_NONE) {
-        LOG_ERROR("Failed to initialize rats tls %#x\n", ret);
+        LOG_ERROR("Failed to initialize rats tls %#x", ret);
         close(sockfd);
         return NULL;
     }
     ret = rats_tls_set_verification_callback(&handle, NULL);
     if (ret != RATS_TLS_ERR_NONE) {
-        LOG_ERROR("Failed to set verification callback %#x\n", ret);
+        LOG_ERROR("Failed to set verification callback %#x", ret);
         goto err;
     }
     ret = rats_tls_negotiate(handle, sockfd);
     if (ret != RATS_TLS_ERR_NONE) {
-        LOG_ERROR("Failed to negotiate %#x\n", ret);
+        LOG_ERROR("Failed to negotiate %#x", ret);
         goto err;
     }
     const char* msg;
@@ -167,19 +167,19 @@ char* get_secret_from_kbs_through_rats_tls(rats_tls_log_level_t log_level,
     size_t len = strlen(msg);
     ret = rats_tls_transmit(handle, (void*)msg, &len);
     if (ret != RATS_TLS_ERR_NONE || len != strlen(msg)) {
-        LOG_ERROR("Failed to transmit %#x\n", ret);
+        LOG_ERROR("Failed to transmit %#x", ret);
         goto err;
     }
     int buff_size = 4096;
     char* buf = malloc(buff_size);
     if (buf == NULL) {
-        LOG_ERROR("Failed to allocate memory\n");
+        LOG_ERROR("Failed to allocate memory");
         goto err;
     }
     len = buff_size;
     ret = rats_tls_receive(handle, buf, &len);
     if (ret != RATS_TLS_ERR_NONE) {
-        LOG_ERROR("Failed to receive %#x\n", ret);
+        LOG_ERROR("Failed to receive %#x", ret);
         free(buf);
         goto err;
     }
@@ -190,7 +190,7 @@ char* get_secret_from_kbs_through_rats_tls(rats_tls_log_level_t log_level,
 
     ret = rats_tls_cleanup(handle);
     if (ret != RATS_TLS_ERR_NONE)
-        LOG_ERROR("Failed to cleanup %#x\n", ret);
+        LOG_ERROR("Failed to cleanup %#x", ret);
 
     close(sockfd);
     return buf;
@@ -206,7 +206,7 @@ int push_wrapkey_to_secret_box(const char* secret) {
     json_error_t error;
     json_t* json_secret = json_loads(secret, 0, &error);
     if (!json_secret) {
-        LOG_ERROR("secret json format error\n");
+        LOG_ERROR("secret json format error");
         return -1;
     }
 
@@ -215,7 +215,7 @@ int push_wrapkey_to_secret_box(const char* secret) {
         const char* key = json_object_iter_key(iter);       //
         json_t* json_value = json_object_iter_value(iter);  //
         if (json_value == NULL) {
-            LOG_WARN("json_object_iter_value fail\n");
+            LOG_WARN("json_object_iter_value fail");
             return -1;
         }
         const char* value;
@@ -225,10 +225,10 @@ int push_wrapkey_to_secret_box(const char* secret) {
                 break;
             }
             default: {
-                LOG_WARN("the value of %s is not string, not support yet\n", key);
+                LOG_WARN("the value of %s is not string, not support yet", key);
             }
         }
-        LOG_DEBUG("key is %s, value is %s\n", key, value);
+        LOG_DEBUG("key is %s, value is %s", key, value);
 
         iter = json_object_iter_next(json_secret, iter);
 
@@ -248,23 +248,23 @@ int push_wrapkey_to_secret_box(const char* secret) {
             strcat(request_buffer, key);
             strcat(request_buffer, "&value=");
             strcat(request_buffer, value);
-            LOG_DEBUG("request body is %s\n", request_buffer);
+            LOG_DEBUG("request body is %s", request_buffer);
 
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request_buffer);
             res = curl_easy_perform(curl);
             if (res != CURLE_OK) {
-                LOG_ERROR("curl_easy_perform() failed: %s \n", curl_easy_strerror(res));
+                LOG_ERROR("curl_easy_perform() failed: %s", curl_easy_strerror(res));
                 return -1;
             }
 
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
             if (http_code != 200) {
-                LOG_ERROR("verify quote from azure failed, http code is ;%ld\n", http_code);
+                LOG_ERROR("verify quote from azure failed, http code is ;%ld", http_code);
                 return -1;
             }
             curl_easy_cleanup(curl);
         } else {
-            LOG_ERROR("init curl failed\n");
+            LOG_ERROR("init curl failed");
             return -1;
         }
     }
@@ -274,14 +274,14 @@ int push_wrapkey_to_secret_box(const char* secret) {
 int main(int argc, char** argv) {
     setvbuf(stdout, NULL, _IONBF, 0);
     char* secret = "";
-    LOG_INFO("try to get key from kbs\n");
+    LOG_INFO("try to get key from kbs");
     char* kbs_endpoint = getenv("kbsEndpoint");
     if (NULL == kbs_endpoint) {
-        LOG_ERROR("kbs mode must config kbsEndpoint\n");
+        LOG_ERROR("kbs mode must config kbsEndpoint");
         return -1;
     }
 
-    LOG_DEBUG("config of kbsEndpoint is %s\n", kbs_endpoint);
+    LOG_DEBUG("config of kbsEndpoint is %s", kbs_endpoint);
 
     char* secret_save_path = NULL;
     char* srv_ip = NULL;
@@ -291,7 +291,7 @@ int main(int argc, char** argv) {
     srv_ip = strtok(kbs_endpoint, ":");
     str_port = strtok(NULL, ":");
     if (NULL == str_port) {
-        LOG_ERROR("kbsEndpoint format error, eg: 127.0.0.1:5443\n");
+        LOG_ERROR("kbsEndpoint format error, eg: 127.0.0.1:5443");
         return -1;
     }
     port = atoi(str_port);
@@ -376,24 +376,24 @@ int main(int argc, char** argv) {
         }
     } while (opt != -1);
 
-    LOG_INFO("Selected log level %d\n", log_level);
+    LOG_INFO("Selected log level %d", log_level);
 
     secret = get_secret_from_kbs_through_rats_tls(log_level, attester_type, verifier_type,
                                                   tls_type, crypto_type, mutual, srv_ip,
                                                   port, appid_flag);
     if (NULL == secret) {
-        LOG_ERROR("get secret from kbs failed\n");
+        LOG_ERROR("get secret from kbs failed");
         return -1;
     }
 
-    LOG_INFO("get secret successful\n");
-    LOG_DEBUG("secret is %s\n", secret);
+    LOG_INFO("get secret successful");
+    LOG_DEBUG("secret is %s", secret);
 
     int code = 0;
     if (secret_save_path != NULL) {
         FILE* file = fopen(secret_save_path, "w");
         if (file == NULL) {
-            LOG_ERROR("Failed to open the file %s\n", secret_save_path);
+            LOG_ERROR("Failed to open the file %s", secret_save_path);
             code = -1;
         } else {
             fputs(secret, file);
@@ -402,7 +402,7 @@ int main(int argc, char** argv) {
     } else {
         int ret = push_wrapkey_to_secret_box(secret);
         if (ret != 0) {
-            LOG_ERROR("push wrapkey to secret box failed\n");
+            LOG_ERROR("push wrapkey to secret box failed");
             code = -1;
         }
     }
