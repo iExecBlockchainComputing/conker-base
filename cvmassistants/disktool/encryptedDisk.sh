36e6f21ed7a468ea77d92c5680952d99927b23c3
aghiles-ait <aghiles.ait-messaoud@iex.ec>
2025-11-03 13:34:14 +0000

    refactor: replace informative or fatal messages generated by echo cmd with corresponding log funtions

--- cvmassistants/disktool/encryptedDisk.sh
+++ cvmassistants/disktool/encryptedDisk.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+log_info() {
+  echo -e "[INFO] $*"
+}
+
+log_fatal() {
+  echo -e "[ERROR] $*" >&2
+  exit 1
+}
+
 # Check required environment variables
 if [ -z "$path" ]; then # /workplace/encryptedData/ 
-    echo 'error:  mount directory is null'
-    exit 1
+    log_fatal "Mount directory is null"
 fi
 
 if [ -z "$disk" ]; then # vda
-    echo 'error: disk dev name is null'
-    exit 1
+    log_fatal "Disk dev name is null"
 fi
 
 diskpath="/dev/$disk" # /dev/vda
@@ -30,17 +37,15 @@ if [ "$keyType" == "none" ]; then
 
     mount "$part_disk" "$path"
     if [ $? -ne 0 ]; then
-        echo 'fail to mount'
-        exit 2
+        log_fatal "Fail to mount"
     else
         ls "$path"
     fi
 else # keyType is not "none"
     if [ -z "$wrapkey" ]; then
-        echo 'error: wrapkey is null'
-        exit 1
+        log_fatal "wrapkey is null"
     fi
-    echo "mount directory is $path"
+    log_info "Mount directory is $path"
 
     mappername="${disk}1"
     if [ ! -d "$path" ]; then
@@ -52,53 +57,48 @@ else # keyType is not "none"
     open_exit_code=$?
     
     if [ $open_exit_code -eq 0 ]; then
-        echo "open luks disk successful"
+        log_info "open luks disk successful"
         cryptsetup close testname 2>/dev/null
     else
-        echo "$open_info"
+        log_info "luksOpen $part_disk: $open_info"
         
         if echo "$open_info" | grep -q "already mapped or mounted"; then
-            echo "already correct mounted"
+            log_info "already correctly mounted"
             exit 0
         elif echo "$open_info" | grep -q "not a valid LUKS device"; then
-            echo "not a LUKS device"
+            log_info "Not a LUKS device"
             echo "$wrapkey" | cryptsetup luksFormat "$part_disk"
             echo "$wrapkey" | cryptsetup open "$part_disk" "$mappername"
             mkfs.ext4 "/dev/mapper/$mappername"
             cryptsetup close "$mappername"
         elif echo "$open_info" | grep -q "doesn't exist or access denied"; then
-            echo "not a exist device"
-            echo "encrypt a new disk of $diskpath"
+            log_info "Not an existing device"
+            log_info "Encrypting new disk of $diskpath"
             echo -e "n\np\n1\n\n\nw\n" | fdisk "$diskpath"
             echo "$wrapkey" | cryptsetup luksFormat "$part_disk"
             echo "$wrapkey" | cryptsetup open "$part_disk" "$mappername"
             mkfs.ext4 "/dev/mapper/$mappername"
             cryptsetup close "$mappername"
         elif echo "$open_info" | grep -q "No key available"; then
-            echo "password error"
-            exit 2
+            log_fatal "Password error"
         else
-            echo "could not know the status"
-            exit 3
+            log_fatal "Could not know the status"
         fi
     fi
     
     # Open the encrypted device
     echo "$wrapkey" | cryptsetup open "$part_disk" "$mappername"
     if [ $? -ne 0 ]; then
-        echo 'fail to cryptsetup open'
-        exit 1
+        log_fatal "Fail to execute cryptsetup open"
     fi
     
     # Mount the device
     mount "/dev/mapper/$mappername" "$path"
     if [ $? -ne 0 ]; then
-        echo 'fail to mount'
-        exit 2
+        log_fatal "Fail to mount"
     else
         ls "$path"
     fi
 fi
 
-echo "mount dir succussful"
-
+log_info "Mount directory is $path"
