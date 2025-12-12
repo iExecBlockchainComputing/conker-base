4b810f50f9b47f882fa8de9b17ff29a2dd6757cf
aghiles-ait <aghiles.ait-messaoud@iex.ec>
2025-12-12 14:45:30 +0100

    simulate error

--- cvmassistants/secretprovider/secret-provider-agent/src/secret_provider_agent.c
+++ cvmassistants/secretprovider/secret-provider-agent/src/secret_provider_agent.c
@@ -235,7 +235,7 @@ int main(int argc, char **argv) {
   const char *secret_save_path = NULL;
   const char *sbs_endpoint = NULL;
   char ip_buf[INET_ADDRSTRLEN];
-  const char *str_port = NULL;
+               const char *str_port = NULL;
   int port;
   int abc;
 
